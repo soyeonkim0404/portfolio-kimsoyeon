fd529795b5949cc1c7147d98d15a8bc4a413a77d
kimsoyeon <soyeon.kim1@emotion.co.kr>
2023-03-14 18:41:44 +0900

    settings

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,16 @@
 import React from "react";
+import styled from "styled-components";
+import { MIXINS } from "@/styles/theme";
 
 const Home: React.FC = () => {
-  return (
-      <div>djfdkslskdfjdfldskd</div>
-  )
+  return <Main>Kim So Yeon</Main>;
 };
+
+const Main = styled.div`
+  width: 100%;
+  height: 100vh;
+  ${MIXINS.flexBox()}
+  font-size: 3rem;
+`;
 
 export default Home;
